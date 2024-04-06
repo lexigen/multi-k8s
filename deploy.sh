368e47cffcf7ddf6fa0e88e3f946ed6b9c9df131
Alexei Maridashvili <a.maridashvili@tvilight.com>
2024-04-06 11:55:44 +0200

    Push to my repos

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t lexigen/multi-client:latest -t lexigen/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t lexigen/multi-server:latest -t lexigen/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t lexigen/multi-worker:latest -t lexigen/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:latest
-docker push stephengrider/multi-worker:$SHA
+docker push lexigen/multi-client:latest
+docker push lexigen/multi-client:$SHA
+docker push lexigen/multi-server:latest
+docker push lexigen/multi-server:$SHA
+docker push lexigen/multi-worker:latest
+docker push lexigen/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=lexigen/multi-client:$SHA
+kubectl set image deployments/server-deployment server=lexigen/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=lexigen/multi-worker:$SHA
